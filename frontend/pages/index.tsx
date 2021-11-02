26abb64c9feb406af1731c48544457e3d8363298
Kyawkhaing444 <kyawkhaing.work@gmail.com>
2021-11-02 11:06:48 +0630

    Added title"

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,1 @@
-import Head from 'next/head';
-
-export default function IndexPage() {
-  return (
-    <>
-      <Head>
-        <title>Index Page</title>
-      </Head>
-      <p>Hello!</p>
-    </>
-  );
-}
+export { default } from './products';
