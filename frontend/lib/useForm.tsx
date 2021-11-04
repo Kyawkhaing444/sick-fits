891770b86ed9e1eddd9998a6bdc05c4804e17199
Kyawkhaing444 <kyawkhaing.work@gmail.com>
2021-11-04 15:30:30 +0630

    Finished styling the form and added image upload

--- frontend/lib/useForm.tsx
+++ frontend/lib/useForm.tsx
@@ -3,20 +3,37 @@ import { ChangeEvent, useState } from 'react';
 interface FormInputType {
   name?: string;
   price?: number;
+  image?: File;
+  description?: string;
 }
 
 export default function useForm(initialState: FormInputType) {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const [inputs, setInputs] = useState(initialState);
-  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, type, value } = e.target;
     let modifitedValue = -1;
+    let fileValue: File | null = null;
     if (type === 'number') {
       modifitedValue = parseInt(value);
     }
+    if (type === 'file') {
+      const { files } = e.target as HTMLInputElement;
+      if (files) {
+        // eslint-disable-next-line prefer-destructuring
+        fileValue = files[0];
+      }
+    }
+    let finalValue: string | number | File = value;
+    if (modifitedValue !== -1) {
+      finalValue = modifitedValue;
+    }
+    if (fileValue) {
+      finalValue = fileValue;
+    }
     setInputs({
       ...inputs,
-      [name]: modifitedValue === -1 ? value : modifitedValue,
+      [name]: finalValue,
     });
   };
   const resetForm = () => {
--- frontend/pages/sell.tsx
+++ frontend/pages/sell.tsx
@@ -1,25 +1,63 @@
+import { ChangeEvent } from 'react';
+import Form from '../components/styles/Form';
 import useForm from '../lib/useForm';
 
 export default function SellPage() {
-  const { inputs, onChangeHandler, resetForm, clearForm } = useForm({
+  const { inputs, onChangeHandler } = useForm({
     name: 'KyawKhaing',
     price: 12345,
+    image: undefined,
+    description: '',
   });
+  const submitHandler = (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // eslint-disable-next-line no-console
+    console.log(inputs);
+  };
   return (
     <div>
       <p>Sell your project!</p>
-      <form>
-        <label htmlFor="name">name</label>
-        <input id="name" name="name" type="text" value={inputs.name} onChange={onChangeHandler} />
-        <label htmlFor="price">price</label>
-        <input id="price" name="price" type="number" value={inputs.price} onChange={onChangeHandler} />
-        <button type="button" onClick={resetForm}>
-          Reset Form
-        </button>
-        <button type="button" onClick={clearForm}>
-          Clear Form
-        </button>
-      </form>
+      <Form onSubmit={submitHandler}>
+        <fieldset>
+          <label htmlFor="image">
+            Image
+            <input id="image" name="image" type="file" onChange={onChangeHandler} />
+          </label>
+          <label htmlFor="name">
+            name
+            <input
+              id="name"
+              name="name"
+              placeholder="name"
+              type="text"
+              value={inputs.name}
+              onChange={onChangeHandler}
+            />
+          </label>
+          <label htmlFor="price">
+            price
+            <input
+              id="price"
+              name="price"
+              placeholder="price"
+              type="number"
+              value={inputs.price}
+              onChange={onChangeHandler}
+            />
+          </label>
+          <label htmlFor="description">
+            price
+            <textarea
+              id="description"
+              name="description"
+              placeholder="Enter description"
+              value={inputs.description}
+              onChange={onChangeHandler}
+            />
+          </label>
+          <button type="submit">+ add product</button>
+        </fieldset>
+      </Form>
     </div>
   );
 }
