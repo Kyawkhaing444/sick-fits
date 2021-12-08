03170adab3802f9225593229b2a466ba73f39956
Kyawkhaing444 <kyawkhaing.work@gmail.com>
2021-12-09 05:25:43 +0630

    feat(Frontend, Added closeCart, and redirect page in checkOut component)

--- frontend/components/CheckOut.tsx
+++ frontend/components/CheckOut.tsx
@@ -4,10 +4,12 @@ import { loadStripe, StripeError } from '@stripe/stripe-js';
 import nProgress from 'nprogress';
 import { FormEvent, useState } from 'react';
 import styled from 'styled-components';
+import router from 'next/router';
 import SickButton from './styles/SickButton';
 import { OrderType } from '../Type/OrderType';
 import { CHECK_OUT_MUTATION } from '../GraphQL/mutation/checkOut';
 import { CurrentUser } from '../GraphQL/query/currentUser';
+import { useCartContext } from '../context/cartContext';
 
 const CheckOutFormStyle = styled.form`
   box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
@@ -26,9 +28,11 @@ export function CheckOutForm() {
   const stripe = useStripe();
   const elements = useElements();
 
-  const [checkOut, { error: GraphQLError }] = useMutation<{ checkout: OrderType }>(CHECK_OUT_MUTATION, {
+  const [checkOut, { error: GraphQLError }] = useMutation<{ checkOut: OrderType }>(CHECK_OUT_MUTATION, {
     refetchQueries: [{ query: CurrentUser }],
   });
+
+  const { closeCart } = useCartContext();
 
   const submitHandler = async (event: FormEvent) => {
     event.preventDefault();
@@ -48,14 +52,21 @@ export function CheckOutForm() {
       setError(paymentMethodResult.error);
     }
     // 5. Send token from step 3 to our keystone server, via custom mutation
-    const order = await checkOut({
+    const { data } = await checkOut({
       variables: {
         token: paymentMethodResult?.paymentMethod?.id,
       },
     });
-    console.log(order);
+    const order = data ? data.checkOut : ({} as OrderType);
     // 6. Change the page to view the order
+    router.push({
+      pathname: `/order/[id]`,
+      query: {
+        id: order.id,
+      },
+    });
     // 7. turn the loader off
+    closeCart();
     setLoader(false);
     nProgress.done();
   };
